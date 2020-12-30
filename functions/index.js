aa68c5f77b52267f814500c1e6502f166e5da826
Filip <filip.knias@o2.pl>
2020-12-30 14:11:21 +0100

    Calculate rating avg and comment count

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 const { db, storage } = require("./admin");
 
 // TODO: Think about REST API on cloud functions and only db snapshots on client
@@ -9,10 +10,51 @@ exports.onPlaceDelete = functions.firestore
     snapshot.data().photos.forEach(async (photo) => {
       await storage.bucket().file(photo.fileName).delete();
     });
-
+    storage.bucket().upload;
     const query = db.collection("ratings").where("placeId", "==", snapshot.id);
     const docs = await query.get();
     docs.forEach(async (doc) => {
       await db.doc(`/ratings/${doc.id}`).delete();
     });
   });
+
+exports.onRatingAdd = functions.firestore
+  .document("ratings/{id}")
+  .onCreate(async (snapshot) => {
+    const placeDoc = db.collection("places").doc(snapshot.data().placeId);
+    const ratingAvg = await calculateRatingAvg(snapshot.data().placeId);
+
+    await placeDoc.update({
+      commentCount: admin.firestore.FieldValue.increment(1),
+      ratingAvg,
+    });
+  });
+
+exports.onRatingDelete = functions.firestore
+  .document("ratings/{id}")
+  .onDelete(async (snapshot) => {
+    const placeDoc = db.collection("places").doc(snapshot.data().placeId);
+    const ratingAvg = await calculateRatingAvg(snapshot.data().placeId);
+
+    await placeDoc.update({
+      commentCount: admin.firestore.FieldValue.increment(-1),
+      ratingAvg,
+    });
+  });
+
+const calculateRatingAvg = async (placeId) => {
+  const query = db.collection("ratings").where("placeId", "==", placeId);
+  const docs = await query.get();
+
+  const ratings = [];
+  docs.forEach((doc) => {
+    ratings.push(doc.data());
+  });
+
+  const ratingsSum = ratings.reduce((total, value) => {
+    return total + value.rate;
+  }, 0);
+  const ratingAvg = ratingsSum / ratings.length;
+
+  return ratingAvg;
+};
